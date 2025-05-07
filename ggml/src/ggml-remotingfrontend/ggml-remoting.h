0e2ae430536076e18a36a36b36e8ac50a06e9ce4
Kevin Pouget <kpouget@redhat.com>
2025-05-07 10:34:24 +0200

    ggml-backend-reg: reindent

--- ggml/src/ggml-remotingfrontend/ggml-remoting.h
+++ ggml/src/ggml-remotingfrontend/ggml-remoting.h
@@ -15,9 +15,10 @@
 #define RMT_LOG_DEBUG(msg) std::cerr << msg << std::endl
 
 struct ggml_backend_remoting_device_context {
-    size_t device;
-    std::string name;
-    std::string description;
+  size_t device;
+  std::string name;
+  std::string description;
+
 };
 
 extern const struct ggml_backend_device_i ggml_backend_remoting_device_i;
@@ -38,24 +39,24 @@ typedef std::shared_ptr<remoting_device_struct> remoting_device;
 typedef std::weak_ptr<remoting_device_struct> remoting_device_ref;
 
 struct ggml_backend_remoting_buffer_context {
-    remoting_device_ref device;
-    remoting_buffer dev_buffer;
-    std::string name;
+  remoting_device_ref device;
+  remoting_buffer dev_buffer;
+  std::string name;
 
-    ggml_backend_remoting_buffer_context(remoting_device_ref device, remoting_buffer&& dev_buffer, std::string& name) :
-        name(name) {
-        UNUSED(device);
-	UNUSED(dev_buffer);
-    }
+  ggml_backend_remoting_buffer_context(remoting_device_ref device, remoting_buffer&& dev_buffer, std::string& name) :
+    name(name) {
+    UNUSED(device);
+    UNUSED(dev_buffer);
+  }
 
-    ~ggml_backend_remoting_buffer_context() {
-        ggml_remoting_destroy_buffer(dev_buffer);
-    }
+  ~ggml_backend_remoting_buffer_context() {
+    ggml_remoting_destroy_buffer(dev_buffer);
+  }
 };
 
 
 struct remoting_context_struct {
-   int i;
+  int i;
 };
 typedef std::shared_ptr<remoting_context_struct> remoting_context;
 typedef std::weak_ptr<remoting_context_struct> remoting_context_ref;
