1e484a1d3c7e3af665e030aafb0bfb76c0ae3d66
Kevin Pouget <kpouget@redhat.com>
2025-05-16 10:59:21 +0200

    build.backend: build llama-run

--- build.backend.sh
+++ build.backend.sh
@@ -4,7 +4,7 @@ rm -f READY_backend FAILED_backend
 echo "int isatty(int fd) { return 1; }" | gcc -O2 -fpic -shared -ldl -o /tmp/isatty.so -xc -
 export LD_PRELOAD=/tmp/isatty.so
 
-cmake --build ../build.remoting-backend --parallel 8 --target llama-cli "$@"
+cmake --build ../build.remoting-backend --parallel 8 --target llama-run "$@"
 
 if [[ $? == 0 ]]; then
     touch READY_backend
