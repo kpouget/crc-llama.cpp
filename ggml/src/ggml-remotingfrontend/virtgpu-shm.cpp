47615869415441b39f24f40811178b64eb69d14b
Kevin Pouget <kpouget@redhat.com>
2025-05-19 11:24:43 +0200

    ggml: src: ggml-remotingfrontend/virtgpu-shm: reduce the verbosity

--- ggml/src/ggml-remotingfrontend/virtgpu-shm.cpp
+++ ggml/src/ggml-remotingfrontend/virtgpu-shm.cpp
@@ -53,7 +53,7 @@ virtgpu_ioctl_map(struct virtgpu *gpu, uint32_t gem_handle, size_t size)
       .handle = gem_handle,
       .pad = 0,
    };
-   printf("virtgpu_ioctl_map(%ld)\n", size);
+
    if (virtgpu_ioctl(gpu, DRM_IOCTL_VIRTGPU_MAP, &args))
       return NULL;
 
@@ -61,7 +61,7 @@ virtgpu_ioctl_map(struct virtgpu *gpu, uint32_t gem_handle, size_t size)
                     args.offset);
    if (ptr == MAP_FAILED)
       return NULL;
-   printf("virtgpu_ioctl_map(%ld) --> %p | %p\n", size, ptr, *(void **)ptr);
+
    return ptr;
 }
 
