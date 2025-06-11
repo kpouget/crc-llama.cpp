d67aed77fd4f53fa5123f5ea75129d8781e8db23
Kevin Pouget <kpouget@redhat.com>
2025-06-11 09:52:53 +0200

    prepare.backend.sh: more flags

--- prepare.backend.sh
+++ prepare.backend.sh
@@ -8,6 +8,8 @@ cmake -S . -B ../build.remoting-backend$FLAVOR \
       -DGGML_REMOTINGBACKEND=ON \
       -DGGML_NATIVE=OFF \
       -DGGML_METAL=ON \
+      -DGGML_BACKEND_DL=OFF \
+      -DLLAMA_CURL=OFF \
       -DGGML_VULKAN=OFF -DVulkan_INCLUDE_DIR=/opt/homebrew/include/ -DVulkan_LIBRARY=/opt/homebrew/lib/libMoltenVK.dylib \
       "$@"
 
